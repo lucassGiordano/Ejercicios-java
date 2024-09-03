d4689bf27156ca032ebefff0af884ad09546819a
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-09-03 14:29:18 -0300

    Create Ejer1.java

---
+++ HolaMundoJava/src/Ejer1.java
@@ -0,0 +1,9 @@
+package hola;
+
+public class hola {
+	public static void main(String[] args) {
+		int a=5;
+		int b=2;
+		System.out.println("suma " +(a+b)+" resta "+(a-b)+" multiplicacion "+(a*b)+" division "+(a/b)+" modulo "+(a%b));
+	}
+}
