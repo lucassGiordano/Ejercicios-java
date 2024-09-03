bf3fae6396fee71bf91e005ab2b373226c3fafc8
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-09-03 14:49:22 -0300

    Create Ejer4.java

---
+++ HolaMundoJava/src/Ejer4.java
@@ -0,0 +1,14 @@
+package Ejer4;
+
+import javax.swing.JOptionPane;//libreria para la ventana output
+
+public class Ejer4 {
+	public static void main(String[] args) {
+			
+		String a = JOptionPane.showInputDialog("tu nombre papa");//ventana con mensaje a dar
+			System.out.println("Bienvenido "+a);
+			JOptionPane.showMessageDialog(null, "bienvenido (ahora en ventanita :D) "+a  );
+			//no entiendo exactamente el porque del null
+
+	}
+}
