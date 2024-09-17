d3fb341bff88343eff35404cacc65d0f7e0d8e3c
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-09-17 15:34:38 -0300

    Create Ejer21.java

---
+++ si/src/Ejer/Ejer21.java
@@ -0,0 +1,17 @@
+package Ejer;
+public class Ejer21 {
+	public static void main(String[] args) {
+		  for (int i=1;i<=100;i++){
+		        int raiz=(int)Math.sqrt(i);
+		        int contador=0;
+		        for (int j=raiz;j>1;j--){
+		            if (i%j==0){
+		                contador++;
+		            }
+		        }
+		        if (contador<1){
+		            System.out.println(i);
+		        }
+		    }	
+	}
+}
