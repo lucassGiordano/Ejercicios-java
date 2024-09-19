b2dc012b3174721b4303272d7cc2691d810509dd
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-09-19 09:30:46 -0300

    Update Ejer22.java

--- si/src/Ejer/Ejer22.java
+++ si/src/Ejer/Ejer22.java
@@ -2,12 +2,7 @@ package Ejer;
 public class Ejer22 {
 	public static void main(String[] args) {
 		  String ea= "La lluvia en Sevilla es una maravilla";
-		  int contador=0;
-		  for(int i=0;i>=ea.length();i++) {
-			  if(ea.charAt(i)=='a'||ea.charAt(i)=='e'||ea.charAt(i)=='i' || ea.charAt(i)=='o' || ea.charAt(i)=='u') {
-				  contador++;
-			  }
-		  }
-		  System.out.println(contador + " vocales tiene la oreaicon");
+		  ea =ea.replaceAll("a","e");
+		  System.out.println(ea);
 	}
 }
