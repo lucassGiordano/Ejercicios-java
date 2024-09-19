fd466ae696380d53caf0dc8a8c63a5794b27068d
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-09-19 09:33:50 -0300

    Create Ejer23.java

---
+++ si/src/Ejer/Ejer23.java
@@ -0,0 +1,8 @@
+package Ejer;
+public class Ejer23 {
+	public static void main(String[] args) {
+		  String ea= "La lluvia en Sevilla es una maravilla";
+		  ea =ea.replaceAll("a","e");
+		  System.out.println(ea);
+	}
+}
