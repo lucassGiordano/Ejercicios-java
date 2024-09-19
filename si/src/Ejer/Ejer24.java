5bf31064f0b9e506188167b8d667881ac488a9f1
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-09-19 09:56:00 -0300

    Create Ejer24.java

---
+++ si/src/Ejer/Ejer24.java
@@ -0,0 +1,9 @@
+package Ejer;
+public class Ejer24 {
+	public static void main(String[] args) {
+		  String ea= "La lluvia en Sevilla es una maravilla";
+		  for(int i=0;i<ea.length();i++) {
+			 System.out.print(((int)ea.charAt(i))+" ");
+		  }
+	}
+}
