6c446eb5532b2b903ad3ab51bafbf0fcf04b483a
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-09-21 11:43:00 -0300

    Create Ejer31.java

---
+++ si/src/Ejer/Ejer31.java
@@ -0,0 +1,12 @@
+package Ejer;
+import java.util.Scanner;
+public class Ejer31 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a="Hola Mundo Esto Es Java";
+        System.out.println("La palabra es: " + a);
+        System.out.println("su longitud es de: "+ a.length()+ " caracteres");
+        sc.close();
+
+    }
+}
