68eb750a1303daaff50c690ee4a17d1f2ba20ac6
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-09-22 18:59:48 -0300

    Create Ejer38.java

---
+++ si/src/Ejer/Ejer38.java
@@ -0,0 +1,30 @@
+package Ejer;
+import java.util.Scanner;
+public class Ejer38 {
+	public static void main(String[] args) {   
+                Scanner sn = new Scanner(System.in);
+                int nume=0;
+                int mayor=0,menor=0,suma=0,sumNpositivos=0,sumNnegativos=0,media=0;
+                int contador = 0;
+                System.out.println("ingresa los numeros que quieras, se terminara el programa cuando el numero sea exactamente -1");
+                do {
+                    nume = sn.nextInt();
+                    if(nume==-1)break;
+                    contador++;
+                    if(nume>mayor) mayor=nume;
+                    if(nume<menor) menor=nume;
+                    suma+= nume;
+                    if(nume>0) sumNpositivos+=nume;
+                    if(nume<0) sumNnegativos+=nume;
+                    
+                }while(nume !=-1);
+                System.out.println("mayor:"+ mayor);
+                System.out.println("menor:"+menor);
+                System.out.println("suma de todos:"+suma);
+                System.out.println("suma positivos"+ sumNpositivos);
+                System.out.println("suma negativos"+ sumNnegativos);
+                System.out.println("media"+(suma/contador));
+
+
+            }
+}
